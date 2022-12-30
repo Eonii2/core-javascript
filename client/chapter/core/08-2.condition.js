cf81e5d0fc8b0df591aff2ae695e528dfa1a1e62
Eonii2 <eoniioff@gmailcom>
2022-12-30 09:39:23 +0900

    username 대소문자 가능하게 변경

--- client/chapter/core/08-2.condition.js
+++ client/chapter/core/08-2.condition.js
@@ -2,11 +2,10 @@
 /* Logical Operators                                                      */
 /* ---------------------------------------------------------------------- */
 
-//과제다
-//마지막 어드민 써야함 Admin admin adMin aDmin 등등 다 인식해야됨
-
 let age = 20;
-if (age >= 14 && age <= 90);
+
+if (age >= 14 && age <= 90) {
+}
 
 let a = 10;
 let b = "";
@@ -27,15 +26,23 @@ let reverseValue = !value;
 let whichFalsy = true && " " && [] && { thisIsFalsy: false };
 
 // 첫번째 Truthy를 찾는 연산 (||)
-let whichTruthy = false || "" || [2, 3].length || { thisIsFalsy: true };
+let whichTruthy = false || "" || [2, 3].length || { thisIsTruthy: true };
 
-let userName = prompt("사용자 아이디를 입력해주세요", "");
-let pw;
+let userName = prompt("사용자 아이디를 입력해주세요.", "");
 
-if (userName === "Admin") {
-  console.log("확인됐습니다.");
-} else if (userName === "" || userName === null) {
+// console.log(userName);
+if (userName?.toLowerCase() === "admin") {
+  let pw = prompt("비밀번호를 입력해 주세요.", "");
+
+  if (pw?.toLowerCase() === "themaster") {
+    console.log("환영합니다.");
+  } else {
+    console.log("취소되었습니다.");
+  }
+} else if (userName.replace(/\s*/g, "") === "" || userName === null) {
+  console.log("취소했습니다.");
+} else {
   console.log("인증되지 않은 사용자 입니다.");
 }
 
-console.log(userName);
+// console.log(userName);
